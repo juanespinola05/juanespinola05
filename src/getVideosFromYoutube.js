ff96fafe50c6428b1da401d7ee963e4897cbffc3
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-20 17:36:17 -0300

    create scrapping function

---
+++ src/getVideosFromYoutube.js
@@ -0,0 +1,36 @@
+import puppeteer from 'puppeteer'
+
+// ? Video: ytd-rich-item-renderer #content
+// ? titulo: yt-formatted-string#video-title
+// ? imagen: #thumbnail yt-image img
+
+const getVideosFromYoutube = async () => {
+  const VIDEOS_URL = 'https://www.youtube.com/@juanespinola05/videos'
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+  await page.goto(VIDEOS_URL, {
+    waitUntil: 'networkidle2'
+  })
+  await page.setViewport({
+    width: 1200,
+    height: 1200
+  })
+
+  const videosData = await page.evaluate(() => {
+    const videosNodes = document.querySelectorAll('ytd-rich-item-renderer #content')
+    const videos = Array.from(videosNodes).slice(0, 3)
+
+    return videos.map(video => {
+      const title = video.querySelector('yt-formatted-string#video-title').textContent
+      const image = video.querySelector('#thumbnail yt-image img').src
+
+      return { title, image }
+    })
+  })
+
+  await browser.close()
+
+  return videosData
+}
+
+export default getVideosFromYoutube
